689ba778b6fdd57fe41829306929df03d20699c1
Aleksandre Tcharbadze <riseofkelevra@gmail.com>
2023-02-23 19:53:10 -0500

    Adding Redux States

--- src/components/Shop/Products.js
+++ src/components/Shop/Products.js
@@ -1,16 +1,36 @@
 import ProductItem from './ProductItem';
 import classes from './Products.module.css';
 
+const DUMMY_PRODUCTS = [
+  { id: 'p1',
+    price: 6,
+    title: 'My First Product',
+    description: 'The first Product'
+  },
+  { id: 'p2',
+    price: 5,
+    title: 'My Second Product',
+    description: 'The second Product'
+  },
+  { id: 'p3',
+    price: 7,
+    title: 'My Third Product',
+    description: 'The third Product'
+  },
+];
+
 const Products = (props) => {
   return (
     <section className={classes.products}>
       <h2>Buy your favorite products</h2>
       <ul>
+        {DUMMY_PRODUCTS.map((product) => (
         <ProductItem
-          title='Test'
-          price={6}
-          description='This is a first product - amazing!'
-        />
+          key={product.id}
+          title={product.title}
+          price={product.price}
+          description={product.description}
+        />))}
       </ul>
     </section>
   );
